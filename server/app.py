65de691b139cd3584069bce0c604c667db2b1151
sandra <muchaisandra002@gmail.com>
2025-08-02 08:47:08 +0300

    Fix route endpoint conflict for React app

--- server/app.py
+++ server/app.py
@@ -1,41 +1,26 @@
 from flask import Flask, send_from_directory
-from flask_sqlalchemy import SQLAlchemy
-from flask_cors import CORS
-from flask_bcrypt import Bcrypt
-from flask_marshmallow import Marshmallow
 import os
-import logging
 
-logging.basicConfig(level=logging.DEBUG)
+app = Flask(__name__, static_folder='../client/build', static_url_path='')
 
-app = Flask(__name__, static_folder='build', static_url_path='')
-
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///jobs.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'supersecret')
-app.config['SESSION_COOKIE_SAMESITE'] = "None"
-app.config['SESSION_COOKIE_SECURE'] = True
-
-CORS(app, supports_credentials=True, origins=[
-    "http://localhost:3000",
-    "https://jobportalappx.netlify.app"
-])
-
-db = SQLAlchemy(app)
-ma = Marshmallow(app)
-bcrypt = Bcrypt(app)
-
-# Register routes AFTER initializing app, db, cors etc.
-from routes import *
-
-# for React front-end (serving static files)
-@app.route('/', defaults={'path': ''}, methods=['GET'], endpoint='serve_react')
-@app.route('/<path:path>', methods=['GET'], endpoint='serve_react_path')
-def serve(path):
-    if path != "" and os.path.exists(os.path.join(app.static_folder, path)):
+def serve_react_file(path='index.html'):
+    """Helper function to serve React files"""
+    try:
+        # Try to serve the requested file from the build directory
         return send_from_directory(app.static_folder, path)
-    else:
+    except FileNotFoundError:
+        # If file not found, serve index.html (for client-side routing)
         return send_from_directory(app.static_folder, 'index.html')
 
+# Route for root URL (/)
+@app.route('/', methods=['GET'])
+def home():
+    return serve_react_file('index.html')
+
+# Route for all other paths (catch-all for React routing)
+@app.route('/<path:path>', methods=['GET'])
+def serve_react_path(path):
+    return serve_react_file(path)
+
 if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    app.run(debug=True)
